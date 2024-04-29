3a096491e1f54e2a67878d9f5f21ab9267ae3746
Devakrishna Ramineni Sai Vijay <RS00847863@TechMahindra.com>
2024-04-29 14:43:47 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,4 @@
+const Parent = React.createElement('div', { id: "parent" }, [React.createElement('div', { id: "child1" }, [React.createElement('h1', { id: "heading" }, 'This is h1 Tag'), React.createElement('h2', { id: "heading" }, 'This is h2 Tag')]), React.createElement('div', { id: "child2" }, [React.createElement('h3', { id: "heading" }, 'This is h3 Tag'), React.createElement('h4', { id: "heading" }, 'This is h4 Tag')])])
+console.log(Parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Parent);
