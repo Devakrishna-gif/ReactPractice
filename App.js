23a304944382b05e83ba6a8c85606b296d65bd74
Devakrishna Ramineni Sai Vijay <RS00847863@TechMahindra.com>
2024-04-30 18:47:17 +0530

    episode-1

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const TitleComponent = () => <h1 className='title'>Component Composotion</h1>
-
-const HeadingComponent = () => {
-    return (
-        <div className='container'>
-            <TitleComponent />
-            <h1 className='heading'>Functional Component</h1>
-        </div>
-    )
-}
+const heading = React.createElement('div', { id: "container" }, [
+    React.createElement('div', { id: "child1" }, [React.createElement('h1', { className: 'heading1' }, "This is Heading1"), React.createElement('h2', { className: "heading2" }, "This is Heading2")]), React.createElement('div', { id: "child1" }, [React.createElement('h3', { className: "heading3" }, "This is Heading3"), React.createElement('h4', { className: "heading4" }, "This is heading4")])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<HeadingComponent />);
+root.render(heading);
