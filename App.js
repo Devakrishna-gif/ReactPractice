b965b0786b683fa730595e50d0799cc049e246af
Devakrishna Ramineni Sai Vijay <RS00847863@TechMahindra.com>
2024-04-30 19:01:14 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-const heading = React.createElement('div', { id: "container" }, [
-    React.createElement('div', { id: "child1" }, [React.createElement('h1', { className: 'heading1' }, "This is Heading1"), React.createElement('h2', { className: "heading2" }, "This is Heading2")]), React.createElement('div', { id: "child1" }, [React.createElement('h3', { className: "heading3" }, "This is Heading3"), React.createElement('h4', { className: "heading4" }, "This is heading4")])
-]);
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement('h1', {}, "Without CDN Links")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
