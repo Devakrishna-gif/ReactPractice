780a7cc5150fcf12500cd349a4407459e90c04f5
Devakrishna Ramineni Sai Vijay <RS00847863@TechMahindra.com>
2024-04-30 12:48:54 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Parent = React.createElement('div', { id: "parent" }, [React.createElement('div', { id: "child1" }, [React.createElement('h1', { id: "heading" }, 'This is h1 Tag'), React.createElement('h2', { id: "heading" }, 'This is h2 Tag')]), React.createElement('div', { id: "child2" }, [React.createElement('h3', { id: "heading" }, 'This is h3 Tag'), React.createElement('h4', { id: "heading" }, 'This is h4 Tag')])])
-console.log(Parent);
+const TitleComponent = () => <h1 className='title'>Component Composotion</h1>
+
+const HeadingComponent = () => {
+    return (
+        <div className='container'>
+            <TitleComponent />
+            <h1 className='heading'>Functional Component</h1>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Parent);
+
+root.render(<HeadingComponent />);
